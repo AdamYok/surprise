f38a65a4b8a464a8498a80c98f2fb973f69cebf2
AdamYok <120728787+AdamYok@users.noreply.github.com>
2024-04-07 08:22:38 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// Firework Effect
+function createFirework() {
+    const colors = ['#ffcc00', '#ff0000', '#00ff00', '#0000ff', '#ff00ff', '#00ffff', '#ffff00'];
+    const color = colors[Math.floor(Math.random() * colors.length)];
+  
+    const firework = document.createElement('div');
+    firework.classList.add('firework');
+    firework.style.background = color;
+    document.body.appendChild(firework);
+  
+    const x = Math.random() * window.innerWidth;
+    const y = Math.random() * window.innerHeight;
+  
+    firework.style.left = `${x}px`;
+    firework.style.top = `${y}px`;
+  
+    setTimeout(() => {
+      firework.remove();
+    }, 1000);
+  }
+  
+  setInterval(createFirework, 200);
+  
